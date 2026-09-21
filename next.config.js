4bd8163c45dd21ebbfa6dfe3b8ec411017fe4032
trendora799-blip <trendora799@gmail.com>
2026-09-21 14:37:38 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200],
+  },
+  compress: true,
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
